8cc99a71996cd0cea7bbec75f179234bde4e04d7
reydajp <hatopopo.amanojaku@gmail.com>
2017-04-05 14:09:10 +0900

    update

--- inc/apply_pdf.sh
+++ inc/apply_pdf.sh
@@ -10,6 +10,7 @@ mkdir -p $CACHE_DIR $BIN_PATH $TMP_PATH
 #WKHTMLTOPDF_URL="http://download.gna.org/wkhtmltopdf/0.12/0.12.2.1/wkhtmltox-0.12.2.1_linux-trusty-amd64.deb"
 # WKHTMLTOPDF_PKG="$CACHE_DIR/wkhtmltopdf.deb"
 WKHTMLTOPDF_PKG="$CACHE_DIR/wkhtmltopdf.tar.xz"
+WKHTMLTOPDF_PATH="$TMP_PATH/wkhtmltopdf"
 # WKHTMLTOPDF_BINARIES="$WKHTMLTOPDF_PATH/usr/local/bin"
 WKHTMLTOPDF_BINARIES="$WKHTMLTOPDF_PATH/wkhtmltox/usr/local/bin"
 
@@ -26,7 +27,7 @@ FONTS_DIR=$(cd "$BP_DIR/user/src/pdf/fonts"; pwd)
 
 echo "       - Unzip package..."
 # dpkg -x $WKHTMLTOPDF_PKG $WKHTMLTOPDF_PATH
-tar xvfJ $WKHTMLTOPDF_PKG -C $WKHTMLTOPDF_PATH
+tar xpvf $WKHTMLTOPDF_PKG -C $WKHTMLTOPDF_PATH
 
 echo "       - Setting permissions"
 chmod +x $WKHTMLTOPDF_BINARIES/*
